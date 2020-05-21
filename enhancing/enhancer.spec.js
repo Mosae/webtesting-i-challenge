add93cbcb5e451872c6e9e1ae0485c3ed5e4ac69
mosaelitsoane@gmail.com <mosaelitsoane@Mosaes-MacBook-Pro.local>
2020-05-21 16:08:46 -0500

    first repair fails and passes

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,15 @@
 const enhancer = require('./enhancer.js');
 // test away!
+describe('enhancement functionality', () => {
+	describe('repair', () => {
+		const item = {
+			name: 'test',
+			durability: 100,
+			enhancement: 55,
+		};
+		it('should return a new item with name durbility & enhancement', () => {
+			const itemToRepair = enhancer.repair(item);
+			expect(itemToRepair.durability).toBe(100);
+		});
+	});
+});
